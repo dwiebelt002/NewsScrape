90e0fdc99fb5ddf5dd0d66accdd36af0656110d0
Devin <devinwiebelt@gmail.com>
2016-08-02 20:04:46 -0400

    server set up, handlebars is being put together

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 var bodyParser = require('body-parser');
-var expressHandlebars = require('express-handlebars');
 var path = require('path');
 var fs = require('fs');
 
@@ -18,12 +17,64 @@ var mongojs = require('mongojs');
 var databaseUrl = "webscrapeDB";
 var collections = ["articles", "comments"];
 
-// Main route (simple Hello World Message)
-app.get('/', function(req, res) {
-  res.send("Hello world");
+//connect mongojs to db variable
+var db = mongojs(databaseUrl, collections);
+db.on('error', function(err) {
+  console.log('Database Error:', err);
 });
 
-var url =""
+//setting up handlebars
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//======================================================================================================
+
+
+// Main route
+app.get('/', function(req, res) {
+
+
+var url = "http://www.ign.com/articles/2016/08/02/death-note-willem-dafoe-joins-cast-as-ryuk?abthid=57a11db0e913ffd032000014"
+
+request(url, function(err, resp, html) {
+	var $ =cheerio.load(html);
+
+	$('html').each(function(i, element){
+
+	var title = $(this).find('.article-headline')
+
+	var title = $('.article-headline').text();
+	var article = $('.article-content').text();
+
+	var results = [];
+	results.push({
+		myTitle: "Article 1",
+		title: title,
+		article: article 
+	});
+
+	console.log(results);
+
+	//var articleNameText = articleName.text();
+
+	//var articleContent = $('.article-content');
+	//var articleContentText = articleContent.text();
+
+	//console.log(articleNameText);
+	//console.log(articleContentText);
+
+	db.articles.insert(results, function(err, docs){
+		if (err) throw err;
+		res.render('index', {
+				articles: docs
+				});
+			});
+		})
+	})
+});
 
 // listen on port 8081
 app.listen(8081, function() {
